82a3f5415493ad289760e02d1e839a7e4f7813ee
sarroyo-omatech <sarroyo@omatech.com>
2023-03-14 11:23:43 +0100

    fixes in getFormData array

--- src/Abstractions/LeadZohoFormAbstraction.php
+++ src/Abstractions/LeadZohoFormAbstraction.php
@@ -14,9 +14,10 @@ abstract class LeadZohoFormAbstraction extends ZohoFormAbstraction implements Le
         $response = $this->client->request(
             'POST',
             $urlClientLeads,
-            ['headers' => ['Content-Type' => 'application/json'],
-            'body' => json_encode(array_filter($this->getFormData()))
-        ]);
+            [
+                'headers' => ['Content-Type' => 'application/json'],
+                'body' => json_encode(array_filter($this->getFormData()))
+            ]);
 
         if (!in_array($response->getStatusCode(), [200, 202])) {
             throw new \Exception($response);
@@ -53,8 +54,9 @@ abstract class LeadZohoFormAbstraction extends ZohoFormAbstraction implements Le
     public function getFormData(): array
     {
         $dataContact = $this->data;
+        $formOrigin = $this->getFuenteDeFormulario();
 
-        return [
+        return array_merge([
             'Date' => $this->created_at->timestamp * 1000,
             'Idioma' => $dataContact['language'] ?? null,
             'Owner' => $this->zohoOwner,
@@ -63,24 +65,20 @@ abstract class LeadZohoFormAbstraction extends ZohoFormAbstraction implements Le
             'Last_Name' => $dataContact['surname'] ?? '-',
             'Full_Name' => ($dataContact['name'] ?? "-") . " " . ($dataContact['surname'] ?? ""),
             'Company' => $dataContact['company'] ?? null,
-            'Description' =>  $data['inquiry'] ?? '**Contacto creado o actualizado desde eDiversa**',
+            'Description' => $data['inquiry'] ?? "**Contacto creado o actualizado desde $formOrigin**",
             'Email' => $dataContact['email'] ?? null,
             'Phone' => $dataContact['phone'] ?? null,
-            'Fuente_de_formulario' => $this->getFuenteDeFormulario(),
-            'Interesado_en' => isset($dataContact['solution']) ? [[
-                'edi' => "EDI",
-                'factura_aapp' => "Factura AAPP",
-                'factura_part' => "Factura particulares",
-                'sii' => "SII",
-                'portal_construc' => "Portal construcción",
-                'silicie' => "SILICIE",
-                'custom_devel' => "Desarrollos a Medida",
-            ][$dataContact['solution']]] : null,
+            'Fuente_de_formulario' => $formOrigin,
             'RGPD' => ($dataContact['checkbox_notifications'] ?? null) == 'on' ? $this->getRgpdValues() : null,
             'Lead_Status' => 'Sin contactar',
             'Ads_Campaign' => "---",
             'Ads_URL' => $this['url'] ?? null,
-        ];
+        ], $this->addExtraFormData($dataContact));
+    }
+
+    protected function addExtraFormData(array $dataContact): array
+    {
+        return [];
     }
 
     public function getNotes(): array
@@ -89,13 +87,7 @@ abstract class LeadZohoFormAbstraction extends ZohoFormAbstraction implements Le
 
         return [
             'Title' => "Comentarios del cliente (web)",
-            'Content' => $dataContact['message'] ?? implode("", [
-                $dataContact['comments'] ?? null,
-                "\n ¿trabajas con EDI? " . (($dataContact['edi'] ?? null) == 1 ? 'SI' : 'NO') . ". \n",
-                " Punto operacional (GLN): " . ($dataContact['gln'] ?? '') . "\n",
-                " Red: " . ($dataContact['red_edi'] ?? '') . "\n",
-                " Interlocutor: " . ($dataContact['interlocutor'] ?? '') . "\n",
-            ])
+            'Content' => $dataContact['message'] ?? $dataContact['comments'] ?? null
         ];
     }
 
